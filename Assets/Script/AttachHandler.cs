2ed32694075875a775daeee26e3af94549adcde4
PosnicAntoine <antoine.posnic@gmail.com>
2019-01-14 18:04:59 +0100

    Added start with virtual button to launch the ball. And attach functionnality

---
+++ Assets/Script/AttachHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachHandler : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onAttachEvent()
+    {
+        Debug.Log("couocu");
+        GameObject mainObj = GameObject.Find("MainImageTarget");
+        if (mainObj != null)
+        {
+            for (int i = 0; i < transform.childCount; i++) {
+                Instantiate(transform.GetChild(i).gameObject, mainObj.transform, true);
+            }
+        }
+
+    }
+}
---
+++ Assets/Script/ButtonHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ButtonHandler : MonoBehaviour , IVirtualButtonEventHandler{
+
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+
+        for (int i = 0; i <vbs.Length; i++)
+        {
+            vbs[i].RegisterEventHandler(this);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("OnButtonPressed::" + vb.VirtualButtonName);
+        obj.GetComponent<Rigidbody>().isKinematic = false;
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("OnButtonReleased::" + vb.VirtualButtonName);
+    }
+}
