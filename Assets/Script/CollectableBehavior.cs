a075573f3f6aec4546b0c644498ec7ac54bbae70
Posnic Antoine <32549290+PosnicAntoine@users.noreply.github.com>
2019-02-23 18:46:54 +0100

    Added Gamemanager and points counting

--- Assets/Script/CollectableBehavior.cs
+++ Assets/Script/CollectableBehavior.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class CollectableBehavior : MonoBehaviour
 {
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = FindObjectOfType<GameManager>();
         
+    }
+    
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.layer == 10)
+        {
+            this.gameObject.SetActive(false);
+            this.gameManager.addOnePoint();
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private int points = 0; 
+    private 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addOnePoint()
+    {
+        this.points++;
+        Debug.Log(points);
+    }
+}
