611f1022288ed6f675c80101a6fb3f7ad98d4c32
Posnic Antoine <32549290+PosnicAntoine@users.noreply.github.com>
2019-02-24 01:57:19 +0100

    Compting points completed

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
-    private int points = 0; 
-    private 
+    private int points = 0;
+    private TextMeshProUGUI pointText;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        pointText = GameObject.Find("PointText").GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -25,6 +26,17 @@ public class GameManager : MonoBehaviour
     public void addOnePoint()
     {
         this.points++;
-        Debug.Log(points);
+        this.updatePointText();
+    }
+
+    public void setPoint(int i)
+    {
+        this.points=i;
+        this.updatePointText();
+    }
+
+    public void updatePointText()
+    {
+        pointText.text = this.points.ToString();
     }
 }
